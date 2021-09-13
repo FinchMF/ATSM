7297a5908bc3de975974b0f0f00d6d20f154349f
Matthew Finch <finchrmatthew@gmail.com>
2021-09-12 23:47:56 -0400

    ola added

--- ola.py
+++ ola.py
@@ -8,24 +8,26 @@ class OLAConverter(Converter):
     def convert_frame(self, analysis_frame: int) -> int:
         return analysis_frame
 
+class OLA:
 
-def ola(channels: int, speed: float = 1., 
-        frame_length: int = 256, analysis_hop: int = None,
-        synthesis_hop: int = None) -> object:
+    @staticmethod
+    def convert(channels: int, speed: float = 1., 
+            frame_length: int = 256, analysis_hop: int = None,
+            synthesis_hop: int = None) -> object:
 
-    if synthesis_hop is None:
-        synthesis_hop: int = frame_length // 2
-    
-    if analysis_hop is None:
-        analysis_hop: int = int(synthesis_hop * speed)
+        if synthesis_hop is None:
+            synthesis_hop: int = frame_length // 2
+        
+        if analysis_hop is None:
+            analysis_hop: int = int(synthesis_hop * speed)
 
-    analysis_window: None = None
-    synthesis_window: np.ndarray = W.hanning(frame_length)
+        analysis_window: None = None
+        synthesis_window: np.ndarray = W.hanning(frame_length)
 
-    converter: Converter = OLAConverter()
+        converter: Converter = OLAConverter()
 
-    return A2S_TSM(
-        converter, channels, frame_length, 
-        analysis_hop, synthesis_hop, 
-        analysis_window, synthesis_window
-    )
+        return A2S_TSM(
+            converter, channels, frame_length, 
+            analysis_hop, synthesis_hop, 
+            analysis_window, synthesis_window
+        )

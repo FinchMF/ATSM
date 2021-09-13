8f605839969bf2980c2b2cc5b8ac4a016a21ec37
Matthew Finch <finchrmatthew@gmail.com>
2021-09-12 23:44:16 -0400

    ola added

---
+++ ola.py
@@ -0,0 +1,31 @@
+
+from ATSM.a2m import A2S_TSM, Converter
+from ATSM.utils.windows import Windows as W
+
+
+class OLAConverter(Converter):
+
+    def convert_frame(self, analysis_frame: int) -> int:
+        return analysis_frame
+
+
+def ola(channels: int, speed: float = 1., 
+        frame_length: int = 256, analysis_hop: int = None,
+        synthesis_hop: int = None) -> object:
+
+    if synthesis_hop is None:
+        synthesis_hop: int = frame_length // 2
+    
+    if analysis_hop is None:
+        analysis_hop: int = int(synthesis_hop * speed)
+
+    analysis_window: None = None
+    synthesis_window: np.ndarray = W.hanning(frame_length)
+
+    converter: Converter = OLAConverter()
+
+    return A2S_TSM(
+        converter, channels, frame_length, 
+        analysis_hop, synthesis_hop, 
+        analysis_window, synthesis_window
+    )
